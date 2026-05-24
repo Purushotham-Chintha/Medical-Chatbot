20ba5f686992bc5d2b7567d79ead0176a6da337a
Purushotham Chintha <puruchintha@gmail.com>
2026-05-24 17:30:27 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from typing import List
+
+setup(
+    name='Medical-Chatbot',
+    version='0.1.0',
+    author="Purushotham Chintha",
+    author_email="puruchintha@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
